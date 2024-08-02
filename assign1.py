2dd4a26f6963fa74f84803afe938407076ab9e7d
Maira-dev21 <mairatariq896@gmail.com>
2024-08-02 13:38:52 -0700

    Create assign1.py

---
+++ assign1.py
@@ -0,0 +1,8 @@
+print(''' My self
+      My name is Maira Tariq. I am currently studying for a BS in Mathematics.
+     I have a keen interest in the IT field because 
+    I believe it will play a crucial role in shaping the modern world in the coming years. 
+     I am excited about the future and the opportunities it brings. 
+       My goal is to bridge my mathematical skills with technological advancements. 
+      Thank you for taking the time to learn more about me.
+      ''')
